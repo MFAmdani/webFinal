5d212ed62de29beaf6b0e8c7ccf5f8cf911d3dd2
mfamdani <farhan_memon1@hotmail.com>
2019-01-08 21:56:27 +0500

    farhan final

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { firebaseConfig } from './credentials';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+      firebase.initializeApp(firebaseConfig);
+  }
+
+}
+
+const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+  if (!user) {
+    this.rootPage = 'LoginPage';
+    unsubscribe();
+  } else {
+    this.rootPage = HomePage;
+    unsubscribe();
+  }
+});
+
---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDYAt6cZUoG6Vn_9_WAm2hPyFYPYWX36Tk",
+    authDomain: "western-triode-200418.firebaseapp.com",
+    databaseURL: "https://western-triode-200418.firebaseio.com",
+    projectId: "western-triode-200418",
+    storageBucket: "western-triode-200418.appspot.com",
+    messagingSenderId: "818431757166"
+};
